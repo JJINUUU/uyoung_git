47e672db0ba750c16fa3d5357f23ff646fa6d1c6
unknown <wooyoung9654@gmail.com>
2019-10-13 16:18:50 +0900

    schedule.v1

---
+++ src/main/java/com/example/androidlab/SearchModel.java
@@ -0,0 +1,20 @@
+package com.example.androidlab;
+
+import ir.mirrajabi.searchdialog.core.Searchable;
+
+public class SearchModel implements Searchable {
+    private String mTitle;
+
+    public SearchModel (String mTitle) {
+        this.mTitle=mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle=mTitle;
+    }
+
+    @Override
+    public String getTitle(){
+        return mTitle;
+    }
+}
