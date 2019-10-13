89926b428ebd4fc3a4819342af2f42f55014a9a9
unknown <wooyoung9654@gmail.com>
2019-10-13 18:14:10 +0900

    schedule.v2

--- lab01/src/main/java/com/example/lab01/MainActivity.java
+++ lab01/src/main/java/com/example/lab01/MainActivity.java
@@ -1,14 +1,14 @@
-package com.example.androidlab;
+package com.example.lab01;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Main2Activity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
     }
 }
